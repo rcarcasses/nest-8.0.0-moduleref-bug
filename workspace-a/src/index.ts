f2dddfcf38a9566c5b99ddd78e561df4ddd475d0
Robert Carcassés Quevedo <carcasses@gmail.com>
2021-07-22 11:00:35 +0100

    fix: add injectable property

--- workspace-a/src/index.ts
+++ workspace-a/src/index.ts
@@ -1,14 +1,15 @@
-import { Module } from '@nestjs/common'
-import {ModuleRef} from "@nestjs/core";
+import { Injectable, Module } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
 
+@Injectable()
 export class DummyClass {
-    constructor(private readonly moduleRef: ModuleRef) {
-        console.log('moduleRef in dummyClass', this.moduleRef)
-    }
+  constructor(private readonly moduleRef: ModuleRef) {
+    console.log('moduleRef in dummyClass', this.moduleRef);
+  }
 }
 
 @Module({
-    providers: [DummyClass],
-    exports: [DummyClass]
+  providers: [DummyClass],
+  exports: [DummyClass],
 })
 export class WorkspaceAModule {}
